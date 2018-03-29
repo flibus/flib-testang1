aad059a601f11cdca418bf91dffcf93d228dc43e
flibus <flibus@gmail.com>
2018-03-29 18:32:58 +0200

    try to solve webpack problem on azure

--- Startup.cs
+++ Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+#if DEBUG
 using Microsoft.AspNetCore.SpaServices.Webpack;
+#endif
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -28,13 +30,16 @@ namespace aspnetcore_angular_sample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
+                #if DEBUG
                 app.UseDeveloperExceptionPage();
                 app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
                 {
                     HotModuleReplacement = true
                 });
+                #endif
             }
             else
             {
